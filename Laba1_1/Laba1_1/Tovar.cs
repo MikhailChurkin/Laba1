8294cd0031f62c2a61e20a13e4ecef51c127b9db
MikhailChurkin <mikhail.churkin.99@mail.ru>
2018-04-23 11:29:29 +0300

    Создание класса

---
+++ Laba1_1/Laba1_1/Tovar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laba1_1
+{
+    class Tovar
+    {
+        class Tovar
+        {
+            private string name;
+            private int kol_vo;
+            private float stoimost;
+        }
+    }
+}
