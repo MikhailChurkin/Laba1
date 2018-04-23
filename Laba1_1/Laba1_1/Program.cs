e6d7a679bbd7b6b3400bd402af184a00ca259f6a
MikhailChurkin <mikhail.churkin.99@mail.ru>
2018-04-23 12:53:47 +0300

    Точка входа в программу

--- Laba1_1/Laba1_1/Program.cs
+++ Laba1_1/Laba1_1/Program.cs
@@ -6,6 +6,10 @@ namespace Laba1_1
 {
     class Program
     {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Tovar ObjTovar = new Tovar();
