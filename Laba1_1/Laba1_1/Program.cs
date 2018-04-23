990b414575527f33998b76710e354029974e1bc7
MikhailChurkin <mikhail.churkin.99@mail.ru>
2018-04-23 12:35:35 +0300

    Создание конструктора по умолчанию

--- Laba1_1/Laba1_1/Program.cs
+++ Laba1_1/Laba1_1/Program.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 namespace Laba1_1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Tovar ObjTovar = new Tovar();
+            Console.WriteLine(ObjTovar);
+            Tovar oobTovar = new Tovar("Asus",2,165);
+            Console.WriteLine(oobTovar);
+            Console.ReadKey();
         }
     }
 }
--- Laba1_1/Laba1_1/Tovar.cs
+++ Laba1_1/Laba1_1/Tovar.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace Laba1_1
 {
-    class Tovar
+    public class Tovar
     {
-        class Tovar
+        private string name;
+        private int kol_vo;
+        private float stoimost;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса Tovar значением по умалчанию
+        /// </summary> 
+
+        public Tovar()
         {
-            private string name;
-            private int kol_vo;
-            private float stoimost;
+            this.name = "";
+            this.kol_vo = 0;
+            this.stoimost = 0;
+
+        }
+        /// <summary>
+        /// Инициализирует новый экземпляр класса Tovar значением 
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="kol_vo">Количество</param>
+        /// <param name="stoimost">Стоимость</param>
+        public Tovar(string Name, int Kol_vo, float Stoimost)
+        {
+            Name = name;
+            Kol_vo = kol_vo;
+            Stoimost = stoimost;
+        }
+        public override string ToString()
+        {
+            return "\n Имя: " + name + "\n Количество: " + kol_vo + "\n Стоимость  : " + stoimost;
         }
     }
 }
